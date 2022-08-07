5828b1abe2b9eb901da0ce945a6d00d729bd072f
Fayjul Islam <fayzulislam9@gmail.com>
2022-08-07 16:37:12 +0600

    postShcema is ready

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = (req, res) => {
-  res.send('Allahu Akbar');
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = (req, res) => {
+  res.send('Post Creation');
 };
