b679ddd306f8a0f7d8e5ad229c3dce2420495d7f
Fayjul Islam <fayzulislam9@gmail.com>
2022-08-07 15:55:06 +0600

    Routes added in backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,28 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
-const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/posts', postRoutes);
 
-app.listen(port, () => {
-  console.log(`http://localhost:5000/ Link is runing ${port}`);
-});
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://sust_cse:sust_cse123@cluster0.rpfm8nj.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
